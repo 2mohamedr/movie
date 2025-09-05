1724beb5c9e1daf37337ba9cb39fa1fec3adb19c
2mohamedr <mohamedtamerx0852@gmail.com>
2025-09-05 03:30:34 +0300

    Revert "* add most of assets in phase1"
    This reverts commit 9f5d04097c93a020deee493e8a8a549105550319.

--- lib/core/theme_manager/color_palette.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class ColorsPallete {
-  static const Color PrimaryColor = Color(0XFFF6BD00);
-  static const Color Dark = Color(0xff121312);
-  static const Color white = Color(0xffffffff);
-
-}
